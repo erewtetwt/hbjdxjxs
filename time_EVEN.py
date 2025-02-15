7a992817d3c5938d33b67d58fb6ce037bcbfaed1
erewtetwt <satress@mail.ru>
2025-02-15 20:32:55 +0300

    EVEN.py

--- time_EVEN.py
+++ time_EVEN.py
@@ -1,7 +1,2 @@
 import timeit
-code_to_test = """
-for i in range(100000):
-    y=10%2
-"""
-execution_time = timeit.timeit(code_to_test, number=1000)
-print(f"Время выполнения: {execution_time / 1000:.8f} секунд")
+print(timeit.timeit("10%2"))
